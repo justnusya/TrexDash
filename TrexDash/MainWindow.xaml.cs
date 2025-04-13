80af20c4ca4fd0c67a8720a7f6c8daaed197236b
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2025-04-13 22:43:30 +0300

    Added main thumbnail to the start window with animated button and some button actions

--- TrexDash/MainWindow.xaml.cs
+++ TrexDash/MainWindow.xaml.cs
@@ -23,6 +23,28 @@ namespace TrexDash
         public MainWindow()
         {
             InitializeComponent();
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(StartThubmnail);
+            MainGrid.Children.Add(StartButton);
+        }
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Remove(StartThubmnail);
+            MainGrid.Children.Remove(StartButton);
+            MainGrid.Children.Add(ChoosingCharBackground);
+            MainGrid.Children.Add(RightButton);
+            MainGrid.Children.Add(LeftButton);
+        }
+
+        private void RightButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Remove(ChoosingCharBackground);
+        }
+
+        private void LeftButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Remove(ChoosingCharBackground);
         }
     }
 }
